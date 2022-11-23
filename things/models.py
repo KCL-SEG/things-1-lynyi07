4efeaaa22dfe854981929db21942a96d50899e0e
Cheng Lyn Yi <lynyi07@gmail.com>
2022-11-23 23:02:06 +0000

    Made migrations for model

--- things/models.py
+++ things/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator
 class Thing(models.Model): 
     name = models.CharField(max_length=30, unique=True,blank=False)
-    description = models.TextField(max_length=120,blank=True, unique=False)
+    description = models.CharField(max_length=120,blank=True, unique=False)
     quantity = models.PositiveIntegerField(unique=False, validators=[MaxValueValidator(100)])       
