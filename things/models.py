bd44912d5d9d3f7c8fb5880589f0afbca54723c9
Cheng Lyn Yi <lynyi07@gmail.com>
2022-11-23 22:55:42 +0000

    Exercise 1,2 and3

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
+class Thing(models.Model): 
+    name = models.CharField(max_length=30, unique=True,blank=False)
+    description = models.TextField(max_length=120,blank=True, unique=False)
+    quantity = models.PositiveIntegerField(unique=False, validators=[MaxValueValidator(100)])       
